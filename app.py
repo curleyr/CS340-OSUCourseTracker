cbbb3867875d4c6480be422383c21c87b9a378d4
Robert Curley <curleyr@classwork.engr.oregonstate.edu>
2024-11-06 03:49:29 -0800

    [feat/courses-and-student-term-plans] adds routes and templates to support courses and student term plans pages and modifies DML.SQL file

--- app.py
+++ app.py
@@ -20,9 +20,42 @@ def viewCourses():
   # Check DB connection and reconnect if needed
   mysql_connection = connectToDB()
 
+  # ---------------------------------
+  # COURSES & PREREQUISITES RETRIEVAL
+  # ---------------------------------
+  # Retrieve all courses and prerequisitesfrom 'Courses' table
+  query = """
+    SELECT
+      c.courseID,
+      CONCAT(c.code, ' ', c.name) AS course,
+      c.credit,  
+      GROUP_CONCAT((SELECT CONCAT(code, ' ', name) FROM Courses WHERE courseID = pc.courseID) ORDER BY pc.courseID SEPARATOR ', ') AS prerequisites 
+      FROM Courses c 
+    LEFT JOIN Prerequisites p   
+      ON c.courseID = p.courseID 
+    LEFT JOIN Courses pc  
+      ON p.prerequisiteCourseID = pc.courseID 
+    GROUP BY c.courseID 
+    ORDER BY c.code ASC
+  """
 
-  
-  return render_template("courses.j2")
+  # Execute query
+  cursor = mysql_connection.cursor()
+  cursor.execute(query)
+  courses = cursor.fetchall()
+
+  # Format courses to pass to template
+  courses = [
+    {
+      "id": row[0],
+      "course": row[1],
+      "credit": row[2],
+      "prerequisites": row[3]
+    }
+    for row in courses
+  ]
+
+  return render_template("courses.j2", courses=courses)
 
 def addCourse():
   pass
@@ -101,7 +134,124 @@ def viewStudentTermPlans():
   # Check DB connection and reconnect if needed
   mysql_connection = connectToDB()
   
-  return render_template("student-term-plans.j2")
+  # ----------------------------
+  # STUDENT TERM PLANS RETRIEVAL
+  # ----------------------------
+  # Retrieve all term plans from 'StudentTermPlans' table
+  query = """
+    SELECT 
+      stp.studentTermPlanID, 
+      stp.studentID, 
+      CONCAT(s.firstName, ' ', s.lastName) as studentName,
+      t.name as termName, 
+      GROUP_CONCAT((SELECT CONCAT(code, ' ', name) FROM Courses WHERE courseID = stpc.courseID) ORDER BY stpc.courseID ASC SEPARATOR ', ') AS courses,
+      CASE WHEN stp.advisorApproved = 1 THEN 'Yes' ELSE 'No' End AS advisorApproved
+    FROM StudentTermPlans stp
+    INNER JOIN Terms t ON stp.termID = t.termID
+	  INNER JOIN Students s ON s.studentID = stp.studentID
+    INNER JOIN StudentTermPlans_has_Courses stpc ON stp.studentTermPlanID = stpc.studentTermPlanID
+    GROUP BY stp.studentTermPlanID
+    ORDER BY stp.studentTermPlanID ASC
+  """
+
+  # Execute query
+  cursor = mysql_connection.cursor()
+  cursor.execute(query)
+  student_term_plans = cursor.fetchall()
+
+  # Format student term plans to pass to template
+  student_term_plans = [
+    {
+      "studentTermPlanID": row[0],
+      "studentID": row[1],
+      "studentName": row[2],
+      "termName": row[3],
+      "courses": row[4],
+      "advisorApproved": row[5]
+    }
+    for row in student_term_plans
+  ]
+
+  # ------------------
+  # STUDENTS RETRIEVAL
+  # ------------------
+  # Retrieve all students from 'Students' table
+  query = """
+    SELECT 
+      CONCAT(lastName, ', ', firstName, ' - ', studentID) AS student,
+      studentID
+    FROM Students
+    ORDER BY lastName ASC
+  """
+
+  # Execute query
+  cursor = mysql_connection.cursor()
+  cursor.execute(query)
+  students = cursor.fetchall()
+
+  # Format student term plans to pass to template
+  students = [
+    {
+      "student": row[0],
+      "studentID": row[1]
+    }
+    for row in students
+  ]
+
+  # ---------------
+  # TERMS RETRIEVAL
+  # ---------------
+  # Retrieve all terms from 'Terms' table
+  query = """
+    SELECT 
+      name, 
+      termID
+    FROM Terms
+    ORDER BY startDate ASC
+  """
+
+  # Execute query
+  cursor = mysql_connection.cursor()
+  cursor.execute(query)
+  terms = cursor.fetchall()
+
+  # Format student term plans to pass to template
+  terms = [
+    {
+      "name": row[0],
+      "termID": row[1]
+    }
+    for row in terms
+  ]
+
+  # -----------------
+  # COURSES RETRIEVAL
+  # -----------------
+  # Retrieve all courses from 'Courses' table
+  query = """
+    SELECT 
+      CONCAT(code, ' ', name) AS course,
+      courseID
+    FROM Courses
+    ORDER BY code ASC
+  """
+
+  # Execute query
+  cursor = mysql_connection.cursor()
+  cursor.execute(query)
+  courses = cursor.fetchall()
+
+  # Format student term plans to pass to template
+  courses = [
+    {
+      "course": row[0],
+      "courseID": row[1]
+    }
+    for row in courses
+  ]
+
+  return render_template("student-term-plans.j2", student_term_plans=student_term_plans, students=students, terms=terms, courses=courses)
+  
 
 def addStudentTermPlan():
   pass
